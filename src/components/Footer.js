028d7a9961a64d8a2f1713a34ac7c2c8748e2392
Varelazi <ilenevarela03@gmail.com>
2023-10-27 09:36:54 -0700

    completed comments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,12 @@
 import React from "react"
-import { Navbar, NavbarBrand } from 'reactstrap'
+import { Navbar,  } from 'reactstrap'
+
 
 const Footer = () => {
     return (
-        <Navbar
-        fixed="bottom"
-        color="black"
-        dark
-      >
-        <p className="copyrights">&copy; 2023 Adrian, Ilene, and Anabella AKA: Housing Hackers</p>
-      </Navbar>
+        <Navbar fixed="bottom" color="black" dark>
+          <p className="copyrights" id="">&copy; 2023 Adrian, Ilene, and Anabella AKA: Housing Hackers</p>
+        </Navbar>
     
     )
 }
